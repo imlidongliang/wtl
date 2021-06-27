172d2a6f33ba5b74fe5c3e308290c8ce12865331
imlidongliang <823037364@qq.com>
2021-06-27 23:09:40 +0800

    首页

--- miniprogram/pages/tabBar/index/index.js
+++ miniprogram/pages/tabBar/index/index.js
@@ -4,6 +4,18 @@ Page({
    * 页面的初始数据
    */
   data: {
+    tabs: [],
+    activeTab: 0,
+  },
+  handleTab(e) {
+    // 传递的参数
+    let query = e.currentTarget.dataset;
+    const index = query.index
+    console.log(index)
+
+    this.setData({
+      activeTab: index
+    })
 
   },
 
@@ -11,34 +23,28 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(getApp().globalData)
+    const tabs = [{
+        title: '推荐',
+        title2: '小程序开发进阶',
+        img: 'http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg',
+        desc: '本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。',
+      },
+      {
+        title: '附近',
+        title2: '微信小程序直播',
+        img: 'http://mmbiz.qpic.cn/sz_mmbiz_png/GEWVeJPFkSHALb0g5rCc4Jf5IqDfdwhWJ43I1IvriaV5uFr9fLAuv3uxHR7DQstbIxhNXFoQEcxGzWwzQUDBd6Q/0?wx_fmt=png',
+        desc: '微信小程序直播系列课程持续更新中，帮助大家更好地理解、应用微信小程序直播功能。',
+      },
+    ]
+    this.setData({
+      tabs
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
 
   },
 
